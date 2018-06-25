19d59a5e654e7227d869f9d2fcdd65b60b1ab489
Linkyu <linkyu@users.noreply.github.com>
2018-06-25 23:41:52 +0200

    (WIP) add jcrop to upload page. see http://jsfiddle.net/w1Lh4w2t/ for an implementation example

--- application/views/admin/upload.php
+++ application/views/admin/upload.php
@@ -8,6 +8,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
     <link rel="shortcut icon" href="<?php echo base_url(); ?>static/images/icon.png">
     <link rel="stylesheet" href="<?php echo base_url(); ?>static/css/style.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>static/css/jcrop/jquery.Jcrop.css" type="text/css" />
 </head>
 <body>
 <header>
@@ -23,34 +24,39 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 <div id="upload_inputs_div">
                     <div>
                         <label>Title
-                            <input>
+                            <input type="text">
                         </label>
                         <label>File
-                            <input>
-                        </label>
-                        <label>Time spent
-                            <input>
-                        </label>
-                        <label>Software
-                            <input>
-                        </label>
-                        <label>Amount of layers
-                            <input>
-                        </label>
-                        <label>Price
-                            <input>
+                            <input type="file" onchange="previewFile()" id="upload_file_input">
                         </label>
                         <label>Description
                             <textarea></textarea>
                         </label>
+                        <br>
+                        <h2>Stats:</h2>
+                        <label>Time spent
+                            <input type="text">
+                        </label>
+                        <label>Software
+                            <input type="text">
+                        </label>
+                        <label>Amount of layers
+                            <input type="text">
+                        </label>
+                        <label>Commission?
+                            <input type="checkbox">
+                        </label>
+                        <label>Price
+                            <input type="text">
+                        </label>
                     </div>
                     <div>
                         <label>Thumbnail
-                            <input>
+                            <img src="" height="200" id="thumbnail_maker" alt="Image preview...">
                         </label>
                     </div>
                 </div>
-                <input type="submit">
+                <input type="submit" value="Upload">
             </form>
         </div>
 
@@ -64,5 +70,61 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         seconds. <?php echo (ENVIRONMENT === 'development') ? 'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?>
     </p>
 </footer>
+<script src="<?php echo base_url(); ?>static/js/jquery-3.3.1.js"></script>
+<script src="<?php echo base_url(); ?>static/js/jcrop/jquery.Jcrop.js"></script>
+<script src="<?php echo base_url(); ?>static/js/jcrop/jquery.color.js"></script>
+<script type="text/javascript">
+    function previewFile() {
+        var preview = document.querySelector('#thumbnail_maker');
+        var file    = document.querySelector('#upload_file_input').files[0];
+        var reader  = new FileReader();
+
+        reader.onloadend = function () {
+            preview.src = reader.result;
+        };
+
+        if (file) {
+            reader.readAsDataURL(file);
+        } else {
+            preview.src = "";
+        }
+
+        create_jcrop();
+    }
+
+    // The variable jcrop_api will hold a reference to the
+    // Jcrop API once Jcrop is instantiated.
+    var jcrop_api;
+
+    function create_jcrop() {
+        jQuery(function($){
+            $('#thumbnail_maker').Jcrop({
+                onRelease: releaseCheck
+            },function(){
+
+                jcrop_api = this;
+                jcrop_api.animateTo([100,100,400,300]);
+
+                // Setup and dipslay the interface for "enabled"
+                $('#can_click,#can_move,#can_size').attr('checked','checked');
+                $('#ar_lock,#size_lock,#bg_swap').attr('checked',false);
+                $('.requiresjcrop').show();
+
+            });
+        });
+    }
+
+    // This function is bound to the onRelease handler...
+    // In certain circumstances (such as if you set minSize
+    // and aspectRatio together), you can inadvertently lose
+    // the selection. This callback re-enables creating selections
+    // in such a case. Although the need to do this is based on a
+    // buggy behavior, it's recommended that you in some way trap
+    // the onRelease callback if you use allowSelect: false
+    function releaseCheck()
+    {
+        jcrop_api.setOptions({ allowSelect: true });
+    }
+</script>
 </body>
 </html>
